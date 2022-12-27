4fe9b843cf7a574b0928499f9d9176f930baa5bc
Alexandre B Barreto <kabart@gmail.com>
2022-12-27 13:23:13 -0300

    update new Java version

---
+++ java/boomslang-c2/src/main/java/br/com/gmltec/boomslangc2/BoomslangC2Application.java
@@ -0,0 +1,13 @@
+package br.com.gmltec.boomslangc2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoomslangC2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoomslangC2Application.class, args);
+	}
+
+}
